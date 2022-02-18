cb7cca4e3e84955b746cb91b96df2e73e8a33ec3
MichalMoudry <moudry.michal@live.com>
2022-02-18 15:17:23 +0100

    Add 1st homework

---
+++ homeworks/01/moum02_moudry.py
@@ -0,0 +1,35 @@
+def author_name() -> str:
+    """Funkce, která vrátí jméno autora."""
+    return "MOUDRÝ Michal"
+
+def author_id() -> str:
+    """Funkce, která vrátí identifikační řetězec autora."""
+    return "MOUM02"
+
+def os() -> str:
+    """Funkce, která vrátí název používaného OS."""
+    return "Windows 11"
+
+def languages() -> str:
+    """Funkce, která vrátí přehled dosavadních
+    zkušeností s programováním."""
+    return """C# - 11 301 řádků (aplikace pro správu osobních financí)
+PHP - 1 100 řádků
+Java - 700 řádků
+Python - 84 řádků
+JavaScript - 3 000 řádků
+R - 73 řádků
+C++ - 104 řádků"""
+
+def my_history() -> str:
+    """Funkce, která vrátí dosavadní zkušenosti
+    se studiem programování."""
+    return """První dva roky na střední škole jsem se učil vývoj
+webových stránek, což zahrnovalo výuku HTML, CSS, PHP (včetně
+tvorby pluginů pro Wordpress) a JS. Třetí a čtvrtý rok střední
+školy jsme se učili C# (včetně tvorby multiplatformních aplikací
+pomocí Xamarinu), a také čtvrtý rok jsme se učili základy jazyka
+Python. Pro svoji dlouhodobou maturitní práci jsem se naučil psát
+aplikace v C# pro UWP. Na vysoké škole jsem se učil programovat v Javě.
+Ve svém volné čase se učím C++ zapomoci language refrence a
+různých tutoriálů."""
