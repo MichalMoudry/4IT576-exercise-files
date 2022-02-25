6df6771a9519697d90828f2dc0881a587a943b0e
MichalMoudry <moudry.michal@live.com>
2022-02-25 13:53:21 +0100

    Modified homework n. 2

--- homework/02/moum02_moudry.py
+++ homework/02/moum02_moudry.py
@@ -8,6 +8,7 @@ jsem se učil programovat v jiných jazycích.
 Co se týče pasáží, které mi dělaly problém, tak zatím jsem
 s žádným tématem neměl problém.
 """
+from cmath import pi
 import dbg; dbg.start_mod(1, __name__)
 from robotcz import *
 ###########################################################################q
@@ -24,20 +25,40 @@ def author_id() -> str:
 def step_back(k:Karel) -> Karel:
     """Funkce, která realizuje se zadaným robotem
 krok vzad a vrátí odkaz na svůj argument."""
+    turn_left(k)
+    turn_left(k)
+    step(k)
     return k
 
 def put_left(k:Karel) -> Karel:
     """Funkce, která realizuje se zadaným robotem
 položení značky na políčko vlevo od zadaného robota
 a vrátí odkaz na svůj argument."""
+    turn_left(k)
+    step(k)
+    put(k)
+    step_back(k)
     return k
 
 def my_function_2(k:Karel) -> Karel:
-    """Funkce, která se zadaným robotem provede vámi
-navrženou činnost a vrátí odkaz na svůj argument."""
+    """Funkce, která realizuje se zadaným robotem otočení
+    doleva, následný posun o jedno políčko, zvednutí značky,
+    vrácení zpět na původní pozici a položení značky na pozici.
+    Ve výsledku jde o vyjmutí značky, která je vlevo od robota."""
+    turn_left(k)
+    step(k)
+    pick(k)
+    step_back(k)
+    put(k)
     return k
 
-print(author_name(), author_id())
+"""
+new_world("0123456789", " .:…∷…:. #")
+k = Karel()
+step(k)
+my_function_2(k)
+input("Press Enter to exit...")
+"""
 
 ###########################################################################q
 dbg.stop_mod(1, __name__)
