758b854c2c46836d1d708c0a9bce08431ce8c13a
MichalMoudry <moudry.michal@live.com>
2022-03-04 12:46:52 +0100

    Prepared basics for homework n. 3

---
+++ homework/03/moum02_moudry.py
@@ -0,0 +1,30 @@
+#Příliš žluťoučký kůň úpěl ďábelské ó - PŘÍLIŠ ŽLUŤOUČKÝ KŮŇ ÚPĚL ĎÁBELSKÉ Ó
+"""
+Zdroje: Prezentace
+
+Problémy: žádné
+
+Připomínky: žádné
+"""
+import dbg; dbg.start_mod(1, __name__)
+from robotcz import *
+###########################################################################q
+
+def author_name() -> str:
+    """
+    Funkce, která vrátí jméno autora.
+    """
+    return "MOUDRÝ Michal"
+
+def author_id() -> str:
+    """
+    Funkce, která vrátí identifikační řetězec autora.
+    """
+    return "MOUM02"
+
+
+# Další požadované funkce
+
+
+###########################################################################q
+dbg.stop_mod(1, __name__)
